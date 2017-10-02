e2bfe0b944e708af6baf7ecbb2b20d4e953b71bf
Mergen Battur <mbattur@gmail.com>
2017-10-02 17:13:13 -0500

    Ongoing

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -2,8 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model() {
+  model: function () {
     return this.store.createRecord('library');
+  },
+
+  setupController: function (controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Create a new library');
+    controller.set('buttonLabel', 'Create');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
   },
 
   actions: {
@@ -13,7 +24,11 @@ export default Ember.Route.extend({
     },
 
     willTransition() {
-      this.controller.get('model').rollbackAttributes();
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
     }
   }
 });
