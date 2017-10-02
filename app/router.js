22073ef3454190b99ce21f2a6fcebd47e5f013d2
Mergen Battur <mbattur@gmail.com>
2017-10-02 11:28:15 -0500

    Ongoing

--- app/router.js
+++ app/router.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+  location: config.locationType
 });
 
 Router.map(function() {
@@ -12,10 +11,12 @@ Router.map(function() {
 
   this.route('admin', function() {
     this.route('invitations');
+    this.route('cantacts');
   });
 
   this.route('libraries', function() {
     this.route('new');
+    this.route('edit', { path: '/:library_id/edit' });
   });
 });
 
